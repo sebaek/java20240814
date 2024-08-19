437d53dd3d876e9558251c7f98031a82d306c10c
sebaek <sebaek@gmail.com>
2024-08-19 16:55:24 +0900

    scanner

--- src/ch02/lecture/p06scanner/C07Scanner.java
+++ src/ch02/lecture/p06scanner/C07Scanner.java
@@ -1,8 +1,18 @@
 package ch02.lecture.p06scanner;
 
+import java.util.Scanner;
+
 public class C07Scanner {
     public static void main(String[] args) {
         // 두 실수를 띄어서 입력 받은 후 더한 값 출력
-        
+        // nextDouble
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("두 수 띄어서 입력>");
+        double v1 = scanner.nextDouble();
+        double v2 = scanner.nextDouble();
+
+        System.out.println("더한 값 = " + (v1 + v2));
+
     }
 }
