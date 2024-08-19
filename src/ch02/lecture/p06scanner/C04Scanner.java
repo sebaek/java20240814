aa6fe5695578d9697643acbac6b4490e165479ce
sebaek <sebaek@gmail.com>
2024-08-19 16:30:53 +0900

    scanner

--- src/ch02/lecture/p06scanner/C04Scanner.java
+++ src/ch02/lecture/p06scanner/C04Scanner.java
@@ -1,8 +1,21 @@
 package ch02.lecture.p06scanner;
 
+import java.util.Scanner;
+
 public class C04Scanner {
     public static void main(String[] args) {
         // 실수 두 개 입력 받아서 더한값 출력하기
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("수1>");
+        String num1 = scanner.nextLine();
+
+        System.out.print("수2>");
+        String num2 = scanner.nextLine();
+
+        double v1 = Double.parseDouble(num1);
+        double v2 = Double.parseDouble(num2);
+
+        System.out.println("두 값의 합>" + (v1 + v2));
     }
 }
