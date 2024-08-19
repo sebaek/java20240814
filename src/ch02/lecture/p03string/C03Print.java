5db7340a0fd545c6ba618b5a92c60dfe3d2d38fd
sebaek <sebaek@gmail.com>
2024-08-19 09:54:16 +0900

    Printf

--- src/ch02/lecture/p03string/C03Print.java
+++ src/ch02/lecture/p03string/C03Print.java
@@ -1,4 +1,11 @@
 package ch02.lecture.p03string;
 
 public class C03Print {
+    public static void main(String[] args) {
+        System.out.printf("형식 문자열 %s%n", "이름등등");
+        System.out.printf("형식 문자열 여러 개 %s, %s%n", "첫번째", "두번째");
+
+        System.out.printf("형식%10s%10s%n", "abc", "def");
+        System.out.printf("형식%-10s%-10s%n", "abc", "def");
+    }
 }
