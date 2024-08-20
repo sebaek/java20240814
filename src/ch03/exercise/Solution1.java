346664ae6304ff99f1a7bee7d90fbe2a13c52b05
sebaek <sebaek@gmail.com>
2024-08-20 11:29:25 +0900

    compare

---
+++ src/ch03/exercise/Solution1.java
@@ -0,0 +1,14 @@
+package ch03.exercise;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+//        int z = (++x) + (y--);
+        x++;
+        int z = x + y;
+        y--;
+        
+        System.out.println("z = " + z); // 31
+    }
+}
