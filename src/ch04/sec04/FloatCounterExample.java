1f93a1d8323b929ec541cf6e3df3fcfcc92aad0a
sebaek <sebaek@gmail.com>
2024-08-21 15:27:41 +0900

    구구단

---
+++ src/ch04/sec04/FloatCounterExample.java
@@ -0,0 +1,12 @@
+package ch04.sec04;
+
+public class FloatCounterExample {
+    public static void main(String[] args) {
+
+
+        for (float x = 0.1F; x <= 1.0F; x += 0.1F) {
+            System.out.println(x);
+
+        }
+    }
+}
