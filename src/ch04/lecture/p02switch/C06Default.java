1d7517ae1a51034e229399186731ba767272e9e2
sebaek <sebaek@gmail.com>
2024-08-21 11:34:17 +0900

    default

--- src/ch04/lecture/p02switch/C06Default.java
+++ src/ch04/lecture/p02switch/C06Default.java
@@ -1,9 +1,26 @@
 package ch04.lecture.p02switch;
 
+import com.sun.security.jgss.GSSUtil;
+
 public class C06Default {
     public static void main(String[] args) {
         // 1~10 (정수)
         int n = (int) (Math.random() * 10) + 1;
         System.out.println("n = " + n);
+
+        switch (n) {
+            case 5:
+            case 10:
+                System.out.println("5의 배수");
+                break;
+            case 3:
+            case 6:
+            case 9:
+                System.out.println("3의 배수");
+                break;
+            default:
+                System.out.println("그외");
+                break; // 마지막 break는 생략 가능
+        }
     }
 }
