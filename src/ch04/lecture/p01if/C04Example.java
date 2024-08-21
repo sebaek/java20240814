8dbcd2b52528ddbd2db37b3a1b7811cabed5ec0d
sebaek <sebaek@gmail.com>
2024-08-21 10:16:40 +0900

    if, else if, else

--- src/ch04/lecture/p01if/C04Example.java
+++ src/ch04/lecture/p01if/C04Example.java
@@ -9,13 +9,13 @@ public class C04Example {
 
         int age = scanner.nextInt();
 
-        if (20 <= age) {
+        if (age >= 20) {
             System.out.println("성인입니다.");
-        } else if (13 <= age) {
+        } else if (age >= 13) {
             System.out.println("청소년입니다.");
-        } else if (6 <= age) {
+        } else if (age >= 6) {
             System.out.println("어린이입니다.");
-        } else if (1 <= age) {
+        } else if (age >= 1) {
             System.out.println("유아입니다.");
         } else {
             System.out.println("나이를 잘못 입력");
