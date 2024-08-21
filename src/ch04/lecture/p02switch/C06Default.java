b31add770b491e6fd42d46917cb4ca5bc0a1cd70
sebaek <sebaek@gmail.com>
2024-08-21 11:31:15 +0900

    default

---
+++ src/ch04/lecture/p02switch/C06Default.java
@@ -0,0 +1,9 @@
+package ch04.lecture.p02switch;
+
+public class C06Default {
+    public static void main(String[] args) {
+        // 1~10 (정수)
+        int n = (int) (Math.random() * 10) + 1;
+        System.out.println("n = " + n);
+    }
+}
