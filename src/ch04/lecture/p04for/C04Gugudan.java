de8d86e6df798b255a2fee36268eafb9c3833e65
sebaek <sebaek@gmail.com>
2024-08-21 15:42:45 +0900

    구구단

--- src/ch04/lecture/p04for/C04Gugudan.java
+++ src/ch04/lecture/p04for/C04Gugudan.java
@@ -1,4 +1,8 @@
 package ch04.lecture.p04for;
 
 public class C04Gugudan {
+    public static void main(String[] args) {
+        // 홀수단만 출력
+        // 3, 5, 7, 9 단 만
+    }
 }
---
+++ src/ch04/lecture/p04for/C05Gugudan.java
@@ -0,0 +1,15 @@
+package ch04.lecture.p04for;
+
+public class C05Gugudan {
+    public static void main(String[] args) {
+        // 9~2단 순으로 출력
+        // 9 X 1 = 9
+        // 9 X 2 = 18
+        // 9 X 3 = 27
+        // ..
+        // 9 X 9 = 81
+        // 8 X 1 = 8
+        // ...
+        // 2 X 9 = 18
+    }
+}
