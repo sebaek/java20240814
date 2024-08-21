1d6f4b28f082415d8f3691cfe1c43fcc17052346
sebaek <sebaek@gmail.com>
2024-08-21 14:41:16 +0900

    for

---
+++ src/ch04/lecture/p04for/C01For.java
@@ -0,0 +1,11 @@
+package ch04.lecture.p04for;
+
+public class C01For {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            // 명령문들...
+            System.out.println("명령문");
+
+        }
+    }
+}
