0dcce0bdccbc7a5e14990f82fd98e8a57a417ab6
sebaek <sebaek@gmail.com>
2024-08-22 09:32:26 +0900

    확인문제 6

---
+++ src/ch04/exercise/Solution61.java
@@ -0,0 +1,13 @@
+package ch04.exercise;
+
+public class Solution61 {
+    public static void main(String[] args) {
+        String s = "";
+
+        for (int i = 0; i < 5; i++) {
+            s += "*";
+            System.out.println(s);
+        }
+
+    }
+}
---
+++ src/ch04/exercise/Solution62.java
@@ -0,0 +1,14 @@
+package ch04.exercise;
+
+public class Solution62 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
