68f99895b0e2dba45b234dd43c51b2601dd70767
sebaek <sebaek@gmail.com>
2024-08-23 09:56:02 +0900

    matrix

---
+++ src/ch05/lecture/p02reference/C03Matrix.java
@@ -0,0 +1,30 @@
+package ch05.lecture.p02reference;
+
+import java.util.Arrays;
+
+public class C03Matrix {
+    public static void main(String[] args) {
+        // matrix (행렬, 2차원 배열)
+        // new int[행의수][열의수]
+        int[][] m1 = new int[3][2];
+
+        /*
+        [0, 0]
+        [0, 0]
+        [0, 0]
+         */
+        m1[0][0] = 55;
+        m1[0][1] = 66;
+
+        m1[1][0] = 77;
+        m1[1][1] = 88;
+
+        m1[2][0] = 99;
+        m1[2][1] = 1010;
+
+
+        System.out.println(Arrays.toString(m1[0]));
+        System.out.println(Arrays.toString(m1[1]));
+        System.out.println(Arrays.toString(m1[2]));
+    }
+}
