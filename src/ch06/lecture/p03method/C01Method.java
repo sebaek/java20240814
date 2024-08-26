7a24dea6c880b0ca8ef24d4c1515c808c4abbba0
sebaek <sebaek@gmail.com>
2024-08-26 16:27:45 +0900

    method

---
+++ src/ch06/lecture/p03method/C01Method.java
@@ -0,0 +1,34 @@
+package ch06.lecture.p03method;
+
+import ch06.lecture.p01class.MyClass02;
+
+public class C01Method {
+    public static void main(String[] args) {
+        MyClass01 a = new MyClass01();
+        MyClass01 b = new MyClass01();
+        // 메소드 실행
+        // 객체명.메소드명()
+        a.on();
+        a.off();
+
+        b.on();
+        b.off();
+
+
+    }
+}
+
+class MyClass01 {
+    // 필드
+
+    // 기능에 대한 코드 작성
+    // methods 작성
+    public void on() {
+        System.out.println("켜다");
+    }
+
+    public void off() {
+        System.out.println("끄다");
+    }
+
+}
