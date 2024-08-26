41a2db176e3e8f0d1e3166a76f96121a2ff9f6d1
sebaek <sebaek@gmail.com>
2024-08-26 15:25:46 +0900

    class

---
+++ src/ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        int[] arr = null;
+//        arr[0] = 10; // NullPointerException
+
+        String s = null;
+//        System.out.println(s.length());
+    }
+}
