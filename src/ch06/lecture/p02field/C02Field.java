9ab20d7692a48b23dc49b33452398ca240147f1f
sebaek <sebaek@gmail.com>
2024-08-26 15:55:45 +0900

    field

---
+++ src/ch06/lecture/p02field/C02Field.java
@@ -0,0 +1,16 @@
+package ch06.lecture.p02field;
+
+public class C02Field {
+}
+
+class MyClass02 {
+    // 필드 정의
+
+    // 필드명 작성관습
+    // lowerCamelCase
+
+    // (책 216쪽 참고)
+    // 필드에 초기값 안 주면
+    // 0, false, null
+
+}
