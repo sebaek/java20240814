de2be59d5f88f9479c73e7fc96d4c4c2e72e1a60
sebaek <sebaek@gmail.com>
2024-08-26 15:40:00 +0900

    field와 methods

--- src/ch06/lecture/p01class/C04Class.java
+++ src/ch06/lecture/p01class/C04Class.java
@@ -1,4 +1,22 @@
 package ch06.lecture.p01class;
 
 public class C04Class {
+
+}
+
+class MyClass04 {
+    // field
+    // 값이 무엇인지?
+    int age = 3;
+    String name = "모니터";
+
+    // method
+    // 기능이 무엇인지?
+    public void on() {
+        System.out.println("켜다");
+    }
+
+    public void off() {
+        System.out.println("끄다.");
+    }
 }
