85e6b07dd7c6cf464f50218ab42a2df48772c976
sebaek <sebaek@gmail.com>
2024-08-28 10:23:54 +0900

    final

---
+++ src/ch06/lecture/p06final/C03Final.java
@@ -0,0 +1,20 @@
+package ch06.lecture.p06final;
+
+import java.util.Arrays;
+
+public class C03Final {
+    public static void main(String[] args) {
+        final int[] a = {3, 4};
+//        a = new int[]{5, 6};// 불가
+        a[0] = 33;
+        System.out.println(Arrays.toString(a));
+        method1(a);
+        System.out.println(Arrays.toString(a));
+
+    }
+
+    static void method1(final int[] a) {
+//        a = new int[]{99, 88}; // 불가
+        a[1] = 99;
+    }
+}
