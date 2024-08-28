b218d7309f53638ceea2df0c7daf0135d5dc36eb
sebaek <sebaek@gmail.com>
2024-08-28 11:13:22 +0900

    static final

---
+++ src/ch06/exercise/p17/Printer.java
@@ -0,0 +1,19 @@
+package ch06.exercise.p17;
+
+public class Printer {
+    static void println(int a) {
+        System.out.println(a);
+    }
+
+    static void println(boolean b) {
+        System.out.println(b);
+    }
+
+    static void println(double d) {
+        System.out.println(d);
+    }
+
+    static void println(String s) {
+        System.out.println(s);
+    }
+}
---
+++ src/ch06/exercise/p17/PrinterExample.java
@@ -0,0 +1,10 @@
+package ch06.exercise.p17;
+
+public class PrinterExample {
+    public static void main(String[] args) {
+        Printer.println(3);
+        Printer.println(true);
+        Printer.println(3.14);
+        Printer.println("홍길동");
+    }
+}
