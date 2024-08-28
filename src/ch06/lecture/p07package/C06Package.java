2d75ce58ba7bb98414d3850e268aafaed72f5333
sebaek <sebaek@gmail.com>
2024-08-28 11:52:15 +0900

    package

--- src/ch06/lecture/p07package/C06Package.java
+++ src/ch06/lecture/p07package/C06Package.java
@@ -1,4 +1,17 @@
 package ch06.lecture.p07package;
 
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+
+import java.io.*; // 같은 패키지의 여러 클래스 한 번에 import
+import java.util.Scanner;
+
 public class C06Package {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+
+        FileInputStream f = new FileInputStream("");
+        FileOutputStream o = new FileOutputStream("");
+    }
 }
