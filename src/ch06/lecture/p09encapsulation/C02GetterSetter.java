9118b00347854c09436392485e531153ccf51677
sebaek <sebaek@gmail.com>
2024-08-28 14:22:13 +0900

    encapsulation

---
+++ src/ch06/lecture/p09encapsulation/C02GetterSetter.java
@@ -0,0 +1,4 @@
+package ch06.lecture.p09encapsulation;
+
+public class C02GetterSetter {
+}
---
+++ src/ch06/sec14/CarExample.java
@@ -0,0 +1,16 @@
+package ch06.sec14;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car c = new Car();
+
+        c.setSpeed(-50);
+        System.out.println("c.getSpeed() = " + c.getSpeed());
+
+        c.setSpeed(60);
+        System.out.println("c.getSpeed() = " + c.getSpeed());
+
+        c.setStop(true);
+        System.out.println("c.getSpeed() = " + c.getSpeed());
+    }
+}
