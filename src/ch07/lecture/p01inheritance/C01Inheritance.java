5ef0aefbc4cdb0fe12a32805ebbbaffd11a357aa
sebaek <sebaek@gmail.com>
2024-08-29 09:29:22 +0900

    상속

---
+++ src/ch07/lecture/p01inheritance/C01Inheritance.java
@@ -0,0 +1,23 @@
+package ch07.lecture.p01inheritance;
+
+public class C01Inheritance {
+    public static void main(String[] args) {
+        SubClass01 o = new SubClass01();
+        o.age = 33;
+        o.method1();
+    }
+}
+
+// 상위클래스, 부모클래스, 슈퍼클래스
+class SuperClass01 {
+    int age;
+
+    void method1() {
+        System.out.println("MyClass01.method1");
+    }
+}
+
+// 하위클래스, 자식클래스, 서브클래스
+class SubClass01 extends SuperClass01 {
+    // age,method1
+}
