422e3b0b862661d879509d780415180dc7d426fd
sebaek <sebaek@gmail.com>
2024-08-29 16:16:15 +0900

    instanceof

---
+++ src/ch07/lecture/p04cast/C04Instanceof.java
@@ -0,0 +1,14 @@
+package ch07.lecture.p04cast;
+
+public class C04Instanceof {
+}
+
+// @formatter:off
+class A{}
+class B extends A{}
+class C extends A{}
+
+class D extends B{}
+class E extends B{}
+
+class F extends C{}
