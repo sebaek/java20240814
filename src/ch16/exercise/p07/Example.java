84d3f8487e0939b11c3e60b23507ec36dfc3658e
sebaek <sebaek@gmail.com>
2024-09-03 12:20:56 +0900

    확인문제 7 / if else

---
+++ src/ch16/exercise/p07/Example.java
@@ -0,0 +1,35 @@
+package ch16.exercise.p07;
+
+public class Example {
+    private static int[] scores = {10, 50, 3};
+
+    public static int maxOrMin(Operator operator) {
+        int result = scores[0];
+
+        for (int score : scores) {
+            result = operator.apply(result, score);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int max = maxOrMin((x, y) -> {
+            if (x > y) {
+                return x;
+            } else {
+                return y;
+            }
+        }); // 50
+        System.out.println("max = " + max);
+
+        int min = maxOrMin((a, b) -> {
+            if (a > b) {
+                return b;
+            } else {
+                return a;
+            }
+        }); // 3
+        System.out.println("min = " + min);
+    }
+}
---
+++ src/ch16/exercise/p07/Operator.java
@@ -0,0 +1,6 @@
+package ch16.exercise.p07;
+
+@FunctionalInterface
+public interface Operator {
+    int apply(int x, int y);
+}
