03e7a38e3537f19a6fa0671ad2d0f814732dbbc0
sebaek <sebaek@gmail.com>
2024-09-03 15:51:39 +0900

    생성자 참조

---
+++ src/ch16/sec05/exam03/Creatable1.java
@@ -0,0 +1,5 @@
+package ch16.sec05.exam03;
+
+public interface Creatable1 {
+    Member create(String id);
+}
---
+++ src/ch16/sec05/exam03/Creatable2.java
@@ -0,0 +1,5 @@
+package ch16.sec05.exam03;
+
+public interface Creatable2 {
+    Member create(String a, String b);
+}
