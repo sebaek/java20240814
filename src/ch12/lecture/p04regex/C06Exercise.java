87cd1e67ce60ce4d66f1aec0be07b268f8c88e4a
sebaek <sebaek@gmail.com>
2024-09-05 12:26:47 +0900

    연습

--- src/ch12/lecture/p04regex/C06Exercise.java
+++ src/ch12/lecture/p04regex/C06Exercise.java
@@ -1,4 +1,11 @@
 package ch12.lecture.p04regex;
 
 public class C06Exercise {
+    public static void main(String[] args) {
+        // 사용자 입력을 받아서
+        // 전화번호 패턴과 같은 지 확인
+        // 010-9999-1111
+        // 02-2345-6789
+        // 031-789-7890
+    }
 }
