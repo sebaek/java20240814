704fc3b74087ca03c81dcabebc3e33271582869f
sebaek <sebaek@gmail.com>
2024-09-05 11:32:38 +0900

    정규표현식

---
+++ src/ch12/lecture/p04regex/C02Exercise.java
@@ -0,0 +1,10 @@
+package ch12.lecture.p04regex;
+
+public class C02Exercise {
+    public static void main(String[] args) {
+        // 사용자 입력을 받아서
+        // 6개의 숫자로 이루어진 문자열인지 확인하는 코드
+
+        
+    }
+}
