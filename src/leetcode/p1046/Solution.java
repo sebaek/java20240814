429f9928efdb08b52e38eb8f3735e8b7a980f17d
sebaek <sebaek@gmail.com>
2024-09-09 10:27:45 +0900

    leetcode 1046

--- src/leetcode/p1046/Solution.java
+++ src/leetcode/p1046/Solution.java
@@ -1,6 +1,7 @@
 package leetcode.p1046;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
@@ -16,16 +17,30 @@ class Solution {
 
         // 리스트에 하나의 돌이 남을 때까지
 
-        //      가장 큰 돌 두개 찾기
-        //          1. sort
-        //          2. 오른쪽 두개
+        while (list.size() > 1) {
+            //      가장 큰 돌 두개 찾기
+            //          1. sort
+            //          2. 오른쪽 두개
+            Collections.sort(list);
 
-        //      큰 돌 두개 꺼내기(지우기)
-        //      두 돌의 무게가 0이 아니면 차이(무게) 돌 다시 넣기
+            //      큰 돌 두개 꺼내기(지우기)
+            Integer big1 = list.remove(list.size() - 1);
+            Integer big2 = list.remove(list.size() - 1);
+
+            //      두 돌의 무게가 0이 아니면 차이(무게) 돌 다시 넣기
+            int diff = big1 - big2;
+
+            if (diff != 0) {
+                list.add(diff);
+            }
+        }
         //
 
 
+        if (list.isEmpty()) {
+            return 0;
+        }
         // 하나 남은 돌의 무게 리턴
-        return 0;
+        return list.get(0);
     }
 }
