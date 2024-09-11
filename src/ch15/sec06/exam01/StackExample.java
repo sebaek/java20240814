00eba7e301f1e0229ba204a531ab6f022e3e53c0
sebaek <sebaek@gmail.com>
2024-09-11 10:21:22 +0900

    leetcode 844

---
+++ src/ch15/sec06/exam01/StackExample.java
@@ -0,0 +1,21 @@
+package ch15.sec06.exam01;
+
+import ch08.sec10.exam01.C;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Coin> box = new Stack<>();
+
+        box.push(new Coin(100));
+        box.push(new Coin(50));
+        box.push(new Coin(500));
+        box.push(new Coin(10));
+
+        while (!box.isEmpty()) {
+            Coin coin = box.pop();
+            System.out.println(coin.getValue() + "원");
+        }
+    }
+}
