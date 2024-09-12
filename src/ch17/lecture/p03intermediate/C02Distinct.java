557ec8049fc8ce948ad0b22ffb4940183fa7a873
sebaek <sebaek@gmail.com>
2024-09-12 09:28:46 +0900

    distinct, filter

---
+++ src/ch17/lecture/p03intermediate/C02Distinct.java
@@ -0,0 +1,15 @@
+package ch17.lecture.p03intermediate;
+
+import java.util.List;
+
+public class C02Distinct {
+    public static void main(String[] args) {
+        // distinct : 중복 제거
+        List<Integer> list = List.of(3, 1, 4, 1, 5, 9, 2);
+        long count = list.stream() // 스트림 만들기
+                .distinct() // 중간연산
+                .count();// 최종연산
+        System.out.println("count = " + count);
+        
+    }
+}
