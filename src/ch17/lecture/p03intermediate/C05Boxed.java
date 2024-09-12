7149e9aaaea1e36933cc14e78d450a949fbe6039
sebaek <sebaek@gmail.com>
2024-09-12 10:22:54 +0900

    boxed

--- src/ch17/lecture/p03intermediate/C05Boxed.java
+++ src/ch17/lecture/p03intermediate/C05Boxed.java
@@ -1,4 +1,27 @@
 package ch17.lecture.p03intermediate;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class C05Boxed {
+    public static void main(String[] args) {
+        // Stream, IntStream, DoubleStream, LongStream
+
+        // 참조타입원소 Stream을 기본타입원소 Stream
+        //  : mapToInt, mapToDouble, mapToLong
+
+        // 기본타입원소 Stream을 참조타입원소 Stream
+        //  : boxed, mapToObj
+
+        int[] nums = {100, 1000, 10, 50, 500};
+        Arrays.stream(nums)
+                .boxed()
+                .forEach(System.out::println);
+
+        Arrays.stream(nums)
+                .mapToObj(n -> n + "원")
+                .forEach(System.out::println);
+
+
+    }
 }
