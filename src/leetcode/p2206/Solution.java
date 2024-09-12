ee63bb8132d63a96808ffa9209c68e36500deb61
sebaek <sebaek@gmail.com>
2024-09-12 16:25:22 +0900

    leetcode 2206

--- src/leetcode/p2206/Solution.java
+++ src/leetcode/p2206/Solution.java
@@ -1,26 +1,17 @@
 package leetcode.p2206;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Solution {
     public boolean divideArray(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        // 배열을 전체 탐색 해서
-        // 각 수가 몇 개인지 저장(map)
-        for (int num : nums) {
-            Integer v = map.getOrDefault(num, 0);
-            map.put(num, v + 1);
-        }
-
-        // map을 전체 탐색해서 value가 홀 수인 것이 있으면
-        // return false
-        for (Integer value : map.values()) {
-            if (value % 2 == 1) {
-                return false;
-            }
-        }
-
-        return true;
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .values()
+                .stream()
+                .allMatch(e -> e % 2 == 0);
     }
 }
