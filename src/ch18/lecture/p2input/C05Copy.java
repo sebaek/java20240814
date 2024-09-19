b0e5829aa536536149d8873178055f6d70b439bc
sebaek <sebaek@gmail.com>
2024-09-19 16:40:22 +0900

    copy

---
+++ src/ch18/lecture/p2input/C05Copy.java
@@ -0,0 +1,27 @@
+package ch18.lecture.p2input;
+
+import java.io.*;
+
+public class C05Copy {
+    public static void main(String[] args) {
+        String original = "C:/Temp/지구온난화.jpg";
+        String target = "C:/Temp/지구온난화2.jpg";
+
+        try {
+            InputStream is = new FileInputStream(original);
+            OutputStream os = new FileOutputStream(target);
+            try (is; os;) {
+                byte[] data = new byte[100]; // 읽은 바이트(데이터) 저장
+                int len = 0; // 읽은 바이트 수 저장
+
+                while ((len = is.read(data)) != -1) {
+                    os.write(data, 0, len);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
