5eb076f9825732e0e48f03cf31deb9cea4489f2b
sebaek <sebaek@gmail.com>
2024-09-19 10:54:07 +0900

    syncronized

--- src/ch14/lecture/p3synchronized/C02Thread.java
+++ src/ch14/lecture/p3synchronized/C02Thread.java
@@ -3,16 +3,22 @@ package ch14.lecture.p3synchronized;
 public class C02Thread {
     private static int value = 0;
 
+    private static Object o = new Object(); // lock, monitor
+
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
-                value++;
+                synchronized (o) {
+                    value++;
+                }
             }
         });
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
-                value++;
+                synchronized (o) {
+                    value++;
+                }
             }
         });
         t1.start();
