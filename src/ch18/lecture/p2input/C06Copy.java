73cf97a299d0b5887539e4856c90f4b1d43a728c
sebaek <sebaek@gmail.com>
2024-09-20 09:23:05 +0900

    copy

--- src/ch18/lecture/p2input/C06Copy.java
+++ src/ch18/lecture/p2input/C06Copy.java
@@ -1,4 +1,22 @@
 package ch18.lecture.p2input;
 
+import java.io.*;
+
 public class C06Copy {
+    public static void main(String[] args) {
+        String origin = "C:/Temp/지구온난화.jpg";
+        String target = "C:/Temp/지구온난화3.jpg";
+
+        try {
+            InputStream is = new FileInputStream(origin);
+            OutputStream os = new FileOutputStream(target);
+
+            try (is; os) {
+                is.transferTo(os);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
