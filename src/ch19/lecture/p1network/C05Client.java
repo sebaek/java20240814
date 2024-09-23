4fd10737aa05924d0b07c84993735678ef7d0e7c
sebaek <sebaek@gmail.com>
2024-09-23 12:24:29 +0900

    채팅

---
+++ src/ch19/lecture/p1network/C05Client.java
@@ -0,0 +1,38 @@
+package ch19.lecture.p1network;
+
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class C05Client {
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("172.30.1.84", 9999);
+        System.out.println("[연결됨!!]");
+
+        // 키보드 입력을 server 로 보내기
+        OutputStream os = socket.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        PrintStream ps = new PrintStream(bos);
+
+        Scanner scanner = new Scanner(System.in);
+
+        try (scanner; ps; bos; os; socket) {
+
+            while (true) {
+                System.out.print(">");
+                String input = scanner.nextLine();
+
+                if (input.equals("종료")) {
+                    break;
+                }
+
+                ps.println(input);
+                ps.flush();
+            }
+            System.out.println("[종료!!!]");
+        }
+    }
+}
---
+++ src/ch19/lecture/p1network/C05Server.java
@@ -0,0 +1,37 @@
+package ch19.lecture.p1network;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class C05Server {
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(9999);
+        try (serverSocket) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Thread thread = new Thread(() -> {
+                    try {
+                        InputStream is = socket.getInputStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader br = new BufferedReader(isr);
+
+                        try (br; isr; socket) {
+                            String line = null;
+                            while ((line = br.readLine()) != null) {
+                                System.out.println(">" + line);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                thread.start();
+            }
+        }
+    }
+}
