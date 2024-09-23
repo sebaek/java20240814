e4aab747ba3f93b23f76ca36bc4106e7a31e9cfb
sebaek <sebaek@gmail.com>
2024-09-23 10:38:31 +0900

    networek

---
+++ src/ch19/lecture/p1network/C03Client.java
@@ -0,0 +1,30 @@
+package ch19.lecture.p1network;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class C03Client {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("172.30.1.84", 9999);
+        OutputStream os = socket.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+
+        String fileName = "C:/Temp/지구온난화.jpg";
+        FileInputStream fis = new FileInputStream(fileName);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        try (bis; fis; bos; os; socket;) {
+            byte[] data = new byte[1000];
+            int length = 0;
+            while ((length = bis.read(data)) != -1) {
+                bos.write(data, 0, length);
+            }
+            bos.flush();
+
+            System.out.println("파일 보내기 완료");
+        }
+    }
+}
